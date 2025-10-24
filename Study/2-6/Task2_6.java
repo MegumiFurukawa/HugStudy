34f482214f80322ef601e884c2268b88739b5fb6
Megumi Furukawa <chihayafull1226@gmail.com>
2025-10-24 09:44:16 +0900

    2-6

---
+++ Study/2-6/Task2_6.java
@@ -0,0 +1,31 @@
+public class Task2_6 {
+    public static void main(String[] args) {
+
+        // 問1
+        int hogeInt = 50;
+
+        // 問2
+        String hogeString = "テスト";
+
+        // 問3
+        // double型の「hogeDouble」という名前の変数を宣言して、そこに「0.8」を格納（初期化）している。
+        double hogeDouble = 0.8;
+
+        // 問4
+        hogeInt = 100;
+
+        // 問5
+        hogeString = "良くできました！";
+
+        // 問6
+        System.out.println(hogeInt);
+
+        // 問7
+        // 変数名hogeString内に格納されている「良くできました！」を表示させている。
+        System.out.println(hogeString);
+
+        // 問8
+        System.out.println(hogeDouble);
+
+    }
+}
