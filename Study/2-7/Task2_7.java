4ab9384ce371b94975d12d70965799d6a9bbe2bb
Megumi Furukawa <chihayafull1226@gmail.com>
2025-10-26 15:35:11 +0900

    2-7

---
+++ Study/2-7/Task2_7.java
@@ -0,0 +1,25 @@
+public class Task2_7 {
+
+    public static void main(String[] args) {
+
+    // 問１
+        String[] country = {"JAPAN", "AMERICA", "KOREA", "ENGLAND"};
+
+    // 問２
+        System.out.println(country.length);
+
+    // 問３
+        String[] sttArray = {"りんご", "もも", "ぶどう"};
+
+    // 問４
+        System.out.println(sttArray[2]);
+
+    // 問５
+        // int型の配列変数intArrayを宣言し、10, 20, 30, 40, 50,で初期化している。
+        int[] intArray = {10, 20, 30, 40, 50};
+
+    // 問６
+        // 配列変数intArray１番目の要素と配列変数intArrayの５番目の要素を合計して出力している。
+            System.out.println(intArray[0] + intArray[4]);
+    }
+}
