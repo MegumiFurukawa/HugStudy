0e43b8312fc7194b449741c71d3092bd32514b7e
Megumi Furukawa <chihayafull1226@gmail.com>
2025-10-28 17:55:07 +0900

    2_8

---
+++ Study/2-8/Task2_8.java
@@ -0,0 +1,37 @@
+public class Task2_8 {
+    public static void main(String[] args) {
+
+        // 問１
+        int plus =  5 + 16;
+
+        // 問２
+        int minus = 27 - 7;
+
+        // 問３
+        // 10と2を掛け合わせた数(20)を変数multiplyに代入している。
+        int multiply = 10 * 2;
+
+
+        // 問４
+        int remi = 20 % 6;
+
+        // 問５
+        // 文字列「中央」を変数townに、文字列「線」を変数lineにそれぞれ代入している。
+        String town = "中央";
+        String line = "線";
+
+        // 問６
+        String train = town + line;
+
+        // 問７
+        System.out.println(plus);
+
+        System.out.println(minus);
+
+        System.out.println(multiply);
+
+        System.out.println(remi);
+
+        System.out.println(train);
+    }
+}
