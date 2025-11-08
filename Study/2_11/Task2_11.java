6504fced27a097e017f61b77fc2f9ba53969b27f
Megumi Furukawa <chihayafull1226@gmail.com>
2025-11-08 10:04:31 +0900

    2_11

---
+++ Study/2_11/Task2_11.java
@@ -0,0 +1,39 @@
+public class Task2_11 {
+    public static void main(String[] args) {
+        // 問１
+        int a = 10;
+        int b = 3;
+
+        System.out.println("和: " + (a + b));
+        System.out.println("差: " + (a - b));
+        System.out.println("積: " + (a * b));
+        System.out.println("商: " + (a / b));
+        System.out.println("余り: " + (a % b));
+
+
+        // 問２
+        for (int i = 1; i <= 20; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int add = sumOddNumbers(numbers);
+        System.out.println("奇数の合計: " + add);
+        }
+
+        public static int sumOddNumbers(int[] numbers) {
+        int add = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+
+        //問題③ 奇数かどうかの判定をするために、for文で取得したnumberを2で割り、余りが0にならないような条件文(if文)を記載して下さい。
+            if(numbers[i] % 2 == 1){
+            add += numbers[i];
+            }
+        }
+        return add;
+        }
+    }
+
